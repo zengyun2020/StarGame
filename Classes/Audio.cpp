7377909f753254f55422e3cec1c3c4940b0e45ea
qz515880 <1195933889@qq.com>
2015-01-16 11:54:05 +0800

    加载音效

--- Classes/Audio.cpp
+++ Classes/Audio.cpp
@@ -25,6 +25,7 @@ void Audio::playClick(){
 	CocosDenshion::SimpleAudioEngine::getInstance()->playEffect("Music/select.ogg",false);
 }
 
+
 void Audio::playCombo(int size){
 	if(size < 5)
 		return;
@@ -36,6 +37,53 @@ void Audio::playCombo(int size){
 		CocosDenshion::SimpleAudioEngine::getInstance()->playEffect("Music/combo_1.ogg",false);
 	}
 }
+
+void Audio::playCoin(){
+	CocosDenshion::SimpleAudioEngine::getInstance()->playEffect("Music/coin.ogg",false);
+}
+
+void Audio::playCoinsIn(){
+	CocosDenshion::SimpleAudioEngine::getInstance()->playEffect("Music/coinsin.ogg",false);
+}
+
+void Audio::playNextGameRound(){
+	CocosDenshion::SimpleAudioEngine::getInstance()->playEffect("Music/NextGameRound.ogg",false);
+}
+
+void Audio::playPropBomb(){
+	CocosDenshion::SimpleAudioEngine::getInstance()->playEffect("Music/Props_Bomb.ogg",false);
+}
+
+void Audio::playPropRainbow(){
+	CocosDenshion::SimpleAudioEngine::getInstance()->playEffect("Music/Props_Rainbow.ogg",false);
+}
+
+void Audio::playPropPlusTime(){
+	CocosDenshion::SimpleAudioEngine::getInstance()->playEffect("Music/Props_Paint.ogg",false);
+}
+
+void Audio::playClear(){
+	CocosDenshion::SimpleAudioEngine::getInstance()->playEffect("Music/clear.ogg",false);
+}
+
+void Audio::playBeep(){
+	CocosDenshion::SimpleAudioEngine::getInstance()->playEffect("Music/beep.ogg",false);
+}
+
+void Audio::playLanding(){
+	CocosDenshion::SimpleAudioEngine::getInstance()->playEffect("Music/landing.ogg",false);
+}
+
+void Audio::playFireWorks(int type){
+	if(type%3==0){
+		CocosDenshion::SimpleAudioEngine::getInstance()->playEffect("Music/fireworks_01.ogg",false);
+	}else if(type%3==1){
+		CocosDenshion::SimpleAudioEngine::getInstance()->playEffect("Music/fireworks_02.ogg",false);
+	}else if(type%3==2){
+		CocosDenshion::SimpleAudioEngine::getInstance()->playEffect("Music/fireworks_03.ogg",false);
+	}
+}
+
 void Audio::prepare(){
 	CocosDenshion::SimpleAudioEngine::getInstance()->preloadBackgroundMusic("Music/music.ogg");
 	CocosDenshion::SimpleAudioEngine::getInstance()->preloadEffect("Music/readygo.ogg");
@@ -43,4 +91,17 @@ void Audio::prepare(){
 	CocosDenshion::SimpleAudioEngine::getInstance()->preloadEffect("Music/combo_1.ogg");
 	CocosDenshion::SimpleAudioEngine::getInstance()->preloadEffect("Music/combo_2.ogg");
 	CocosDenshion::SimpleAudioEngine::getInstance()->preloadEffect("Music/combo_3.ogg");
+
+	CocosDenshion::SimpleAudioEngine::getInstance()->preloadEffect("Music/coin.ogg");
+	CocosDenshion::SimpleAudioEngine::getInstance()->preloadEffect("Music/coinsin.ogg");
+	CocosDenshion::SimpleAudioEngine::getInstance()->preloadEffect("Music/NextGameRound.ogg");
+	CocosDenshion::SimpleAudioEngine::getInstance()->preloadEffect("Music/Props_Bomb.ogg");
+	CocosDenshion::SimpleAudioEngine::getInstance()->preloadEffect("Music/Props_Rainbow.ogg");
+	CocosDenshion::SimpleAudioEngine::getInstance()->preloadEffect("Music/Props_Paint.ogg");
+	CocosDenshion::SimpleAudioEngine::getInstance()->preloadEffect("Music/clear.ogg");
+	CocosDenshion::SimpleAudioEngine::getInstance()->preloadEffect("Music/beep.ogg");
+	CocosDenshion::SimpleAudioEngine::getInstance()->preloadEffect("Music/landing.ogg");
+	CocosDenshion::SimpleAudioEngine::getInstance()->preloadEffect("Music/fireworks_01.ogg");
+	CocosDenshion::SimpleAudioEngine::getInstance()->preloadEffect("Music/fireworks_02.ogg");
+	CocosDenshion::SimpleAudioEngine::getInstance()->preloadEffect("Music/fireworks_03.ogg");
 }
